659f50564c3813dd2af0c4d42215e6c499c89195
vdso10 <vdso@bol.com.br>
2020-11-12 16:02:06 -0300

    commit1

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        <p>Direitos Reservados</p>
+    </footer>
+    
+</template>
+
+<script>
+export default {
+    name: "TheFooter"
+    
+}
+</script>
+
+<style scoped>
+footer {
+    background: #87f;
+    padding: 25px;
+    text-align: center;
+    position: relative;
+    top: 75px;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    
-  </div>
+  <section>
+    <h1>👍 Compra ou Venda 👎</h1>
+    <ProdutosBuscar/>
+    <ProdutosLista/>
+  </section>
 </template>
 
 <script>
-
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue"
+import ProdutosLista from "@/components/ProdutosLista.vue"
 
 export default {
   name: 'Home',
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
  
 }
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
