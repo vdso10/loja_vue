34ce5a5cb3220b21efae4354134b8cd9f11de208
vdso10 <vdso@bol.com.br>
2020-11-13 13:59:57 -0300

    commit2

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,38 +1,53 @@
 <template>
-<header>
-    <nav>
-        <router-link  to="/" class="logo">
-            <img src="@/assets/ranek.svg" alt="Ranek">
-        </router-link>
-        <router-link class="btn" to="/login">Vender / Login</router-link>
-    </nav>
-</header>
+  <form>
+    <input name="busca" id="busca" type="text" v-model="busca" placeholder="Buscar...">
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+  </form>
 </template>
 
 <script>
 export default {
-    name: "TheHeader"
-}
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
+};
 </script>
 
 <style scoped>
-nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 15px 20px;
-    box-shadow: 0 2px 4px rgba(30,60,90, 0.1);
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
 }
 
-.logo {
-    padding: 10px 0;
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
 }
 
-.logo img {
-    width: 90px;    
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
 }
 
-
-
-
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
 </style>
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,38 +1,99 @@
 <template>
-<header>
-    <nav>
-        <router-link  to="/" class="logo">
-            <img src="@/assets/ranek.svg" alt="Ranek">
+  <section class="produtos-container">
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco">{{produto.preco}}</p>
+          <h2 class="titulo">{{produto.nome}}</h2>
+          <p>{{produto.descricao}}</p>
         </router-link>
-        <router-link class="btn" to="/login">Vender / Login</router-link>
-    </nav>
-</header>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
+
 export default {
-    name: "TheHeader"
-}
+  data() {
+    return {
+      produtos: null,
+      produtosPorPagina: 9
+    };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
+  },
+  methods: {
+    getProdutos() {
+      api.get(this.url).then(response => {
+        this.produtos = response.data;
+      });
+    }
+  },
+  watch: {
+    url() {
+      this.getProdutos();
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+};
 </script>
 
 <style scoped>
-nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 15px 20px;
-    box-shadow: 0 2px 4px rgba(30,60,90, 0.1);
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
 }
 
-.logo {
-    padding: 10px 0;
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
 }
 
-.logo img {
-    width: 90px;    
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
 }
 
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
 
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
 
+.titulo {
+  margin-bottom: 10px;
+}
 
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
